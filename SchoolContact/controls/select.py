4883d4bec4c68ce2427e6e36d853719d67b5e5da
shaojunmei <Juingya@gmail.com>
2014-03-03 17:47:45 +0800

    sjm

--- SchoolContact/controls/select.py
+++ SchoolContact/controls/select.py
@@ -12,9 +12,21 @@ def to_select_page():
                            marked='yes')
 
 
-def select_student_by_industry():
+def select_student():
     '''根据行业查找'''
-    industry = request.args.get('industry') # 获取web端传如参数
-    student_all = get_student_by_industry(industry)
-    return render_template('all_student.html',
-                           student_all=student_all)
+    #industry = request.args.get('industry') # 获取web端传如参数
+    #student_all = get_student_by_industry(industry)
+    #return render_template('all_student.html',
+     #                      student_all=student_all)
+
+    trade = request.form.get('trade')
+    enter_time = request.form.get('e_time')
+    student_selected_count = db.query(StudentsClass)\
+        .join(IndustryClass).filter(StudentsClass.stu_enter_time==enter_time and IndustryClass.trade ==trade).count()
+    if student_selected_count > 1:
+        student_selected =  db.query(StudentsClass)\
+        .join(IndustryClass).filter(StudentsClass.stu_enter_time==enter_time and IndustryClass.trade ==trade).all()
+    else:
+         student_selected = db.query(StudentsClass)\
+        .join(IndustryClass).filter(StudentsClass.stu_enter_time==enter_time and IndustryClass.trade ==trade).first()
+    return render_template('search.html',student_selected = student_selected)
