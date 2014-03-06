b71b25551fff32abe6f0930a9960888dab915921
shaojunmei <Juingya@gmail.com>
2014-03-06 23:13:39 +0800

    关键字查询

--- SchoolContact/services/select_service.py
+++ SchoolContact/services/select_service.py
@@ -3,7 +3,8 @@
 from SchoolContact.models.students import StudentsClass as Student
 from SchoolContact.models.industry import Industry
 from SchoolContact import db
-from sqlalchemy import or_,and_
+from sqlalchemy import or_,and_,LargeBinary
+
 
 def get_student_all():
     '''获取所有学生'''
@@ -34,18 +35,50 @@ def select_student_first(enter_time,industry_id):
 
 def vague_count(para):
     #模糊查询
-
-    para_int = int(para)
-    student_count = Student.query.filter(or_(Student.stu_name.like('%'+para+'%'),Student.stu_enter_time.like('%'+para+'%'),Student.industry_id.like('%'+para_int+'%'),Student.stu_company.like('%'+para+'%'),Student.stu_position.like('%'+para+'%'))).count()
+    student_count = get_student_count(para)
+    if student_count == 0:
+        student_count= get_industry_count(para)
+    else:
+        pass
     return student_count
+
+def get_student_count(para):
+    #其他条件查询
+     student_count = Student.query.filter(or_(Student.stu_name.like('%'+para+'%'),Student.stu_enter_time.like(LargeBinary('%'+para+'%')),Student.stu_company.like('%'+para+'%'),Student.stu_position.like('%'+para+'%'))).count()
+     return  student_count
+
+def get_industry_count(para):
+    #根据行业查询
+       industry = Industry.query.filter(Industry.industry_name.like('%'+para+'%')).first()
+       id = industry.id
+       student_count = Student.query.filter(Student.industry_id == id).count()
+       return student_count
+
+def get_by_industry(para):
+
+     industry = Industry.query.filter(Industry.industry_name.like('%'+para+'%')).first()
+     id = industry.id
+     student_count = Student.query.filter(Student.industry_id == id).count()
+     if student_count > 1:
+         student = Student.query.filter(Student.industry_id == id).all()
+     else:
+         student = Student.query.filter(Student.industry_id == id).first()
+
+     return student
 
 def vague_first(para):
     #查询到一个
-     para_int = int(para)
-     student_first =Student.query.filter(or_(Student.stu_name.like('%'+para+'%'),Student.stu_enter_time.like('%'+para+'%'),Student.industry_id.like('%'+para_int+'%'),Student.stu_company.like('%'+para+'%'),Student.stu_position.like('%'+para+'%'))).first()
+     student_count = get_student_count(para)
+     if student_count < 1:
+         student_first = get_by_industry(para)
+     else:
+         student_first =Student.query.filter(or_(Student.stu_name.like('%'+para+'%'),Student.stu_enter_time.like (LargeBinary('%'+para+'%')),Student.stu_company.like('%'+para+'%'),Student.stu_position.like('%'+para+'%'))).first()
      return student_first
 def vague_all(para):
      #查询到多条
-     para_int = int(para)
-     student_all =Student.query.filter(or_(Student.stu_name.like('%'+para+'%'),Student.stu_enter_time.like('%'+para+'%'),Student.industry_id.like('%'+para_int+'%'),Student.stu_company.like('%'+para+'%'),Student.stu_position.like('%'+para+'%'))).all()
+     student_count = get_student_count(para)
+     if student_count < 1:
+         student_all = get_by_industry(para)
+     else:
+         student_all =Student.query.filter(or_(Student.stu_name.like('%'+para+'%'),Student.stu_enter_time.like (LargeBinary('%'+para+'%')),Student.stu_company.like('%'+para+'%'),Student.stu_position.like('%'+para+'%'))).all()
      return student_all
