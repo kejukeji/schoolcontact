5448212d718f0187eef24df3f1c4f090bf24171e
shaojunmei <Juingya@gmail.com>
2014-04-08 16:52:37 +0800

    sjm

--- SchoolContact/views/concerned.py
+++ SchoolContact/views/concerned.py
@@ -5,6 +5,7 @@ from SchoolContact.models.user_attention import *
 from SchoolContact.services.student_service import *
 
 def concerned_page(stu_id):
+    current_page = request.args.get('current_page')
     user_concerned_count = get_user_by_stu_id_count(stu_id)
     user_concerned = get_user_by_stu_id(stu_id)
     students =[]
@@ -16,7 +17,8 @@ def concerned_page(stu_id):
         students = get_stu_by_id(user_concerned.followers)
     else:
         pass
-
+    per_page=5
+    max_page= (user_concerned_count-1)/per_page+1
     return render_template('/concerned_page.html',students = students,user_concerned_count=user_concerned_count)
 
 
