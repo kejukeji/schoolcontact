c267f465c48cbfd585176d5d892aa62315ffc67f
shaojunmei <Juingya@gmail.com>
2014-04-13 11:23:51 +0800

    sjm关注分页

--- SchoolContact/views/concerned.py
+++ SchoolContact/views/concerned.py
@@ -5,21 +5,23 @@ from SchoolContact.models.user_attention import *
 from SchoolContact.services.student_service import *
 
 def concerned_page(stu_id):
-    current_page = request.args.get('current_page')
+    current_page = request.args.get('current_page') #当前页
+    page_number = request.args.get('page_type')        #翻页参数
+
     if current_page == None:
         current_page =1
     else:
        current_page = int(current_page)
     user_concerned_count = get_user_by_stu_id_count(stu_id)
     user_concerned = get_user_by_stu_id(stu_id)
-    students =[]
+    students_all =[]
     if user_concerned_count > 1:
         for i in user_concerned:
             student = get_stu_by_id(i.followers)
-            students.append(student)
-            #fro =(current_page-1) * 5 + 1
-            #to = current_page * 5
-            #students = students_all[fro,to]
+            students_all.append(student)
+            fro =(current_page-1) * 5 + 1
+            to = current_page * 5+1
+            students = students_all[fro:to]
 
     elif user_concerned_count ==1:
           students = get_stu_by_id(user_concerned.followers)
@@ -27,10 +29,15 @@ def concerned_page(stu_id):
         pass
     per_page=5
     max_page= (user_concerned_count-1)/per_page+1
-    if current_page > max_page or current_page < 0:
+    if page_number == 'up':
+        current_page = int(current_page) -1
+    else:
+        current_page = int(current_page) +1
+    if current_page > max_page or current_page < 1:
         current_page = 1
     else:
         pass
+
 
     return render_template('/concerned_page.html',students = students,
                                                        user_concerned_count=user_concerned_count,
